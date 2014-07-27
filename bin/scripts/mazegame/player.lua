cccb80ea4f47b522b0f50dda70f8639a3ed8bf5e
Eigen Lenk <eigen@tlu.ee>
2014-07-27 19:02:44 +0300

    Fixed mazedemo movement.

--- bin/scripts/mazegame/player.lua
+++ bin/scripts/mazegame/player.lua
@@ -2,15 +2,20 @@
 
 Player.X = 1
 Player.Y = 1
-Player.MoveDirection = 0
+Player.MoveUp = false
+Player.MoveDown = false
+Player.MoveLeft = false
+Player.MoveRight = false
 
 local moveCounter = 0.0
 
 Player.update = function (frameTime)
-	moveCounter = moveCounter + frameTime
-	if moveCounter >= 0.4 then
-		Player.move()
-		moveCounter = 0.0
+	if Player.MoveUp or Player.MoveDown or Player.MoveRight or Player.MoveLeft then
+		moveCounter = moveCounter + frameTime
+		if moveCounter >= 0.11 then
+			Player.move()
+			moveCounter = 0.0
+		end
 	end
 end
 
@@ -30,37 +35,33 @@ Player.handleKey = function (keyCode, pressed)
 	-- Up
 	if keyCode == 73 then
 		if pressed == true and Player.MoveDirection ~= 1 then
-			Player.MoveDirection = 1
-			Player.move()
-		else
-			Player.resetMove()
+			Player.MoveUp = true
+		elseif pressed == false then
+			Player.MoveUp = false
 		end
 
 	-- Right
 	elseif keyCode == 72 then
 		if pressed == true and Player.MoveDirection ~= 2 then
-			Player.MoveDirection = 2
-			Player.move()
-		else
-			Player.resetMove()
+			Player.MoveRight = true
+		elseif pressed == false then
+			Player.MoveRight = false
 		end
 	
 	-- Down
 	elseif keyCode == 74 then
 		if pressed == true and Player.MoveDirection ~= 3 then
-			Player.MoveDirection = 3
-			Player.move()
-		else
-			Player.resetMove()
+			Player.MoveDown = true
+		elseif pressed == false then
+			Player.MoveDown = false
 		end
 
 	-- Left
 	elseif keyCode == 71 then
 		if pressed == true and Player.MoveDirection ~= 4 then
-			Player.MoveDirection = 4
-			Player.move()
-		else
-			Player.resetMove()
+			Player.MoveLeft = true
+		elseif pressed == false then
+			Player.MoveLeft = false
 		end
 	end
 	
@@ -68,7 +69,10 @@ end
 
 Player.resetMove = function ()
 	moveCounter = 0.0
-	Player.MoveDirection = 0
+	Player.MoveUp = false
+	Player.MoveDown = false
+	Player.MoveLeft = false
+	Player.MoveRight = false
 end
 
 Player.move = function ()
@@ -76,19 +80,20 @@ Player.move = function ()
 	local newPosition = { x = Player.X, y = Player.Y }
 
 	-- Up
-	if Player.MoveDirection == 1 then
+	if Player.MoveUp == true then
 		newPosition.y = newPosition.y - 1
 
+	-- Down
+	elseif Player.MoveDown == true then
+		newPosition.y = newPosition.y + 1
+	end
+		
 	-- Right
-	elseif Player.MoveDirection == 2 then
+	if Player.MoveRight == true then
 		newPosition.x = newPosition.x + 1
 
-	-- Down
-	elseif Player.MoveDirection == 3 then
-		newPosition.y = newPosition.y + 1
-		
 	-- Left
-	elseif Player.MoveDirection == 4 then
+	elseif Player.MoveLeft == true then
 		newPosition.x = newPosition.x - 1
 	end
 
